mk-pmb/anno-server-22

Anno routes: Switch argument order by concepts' extent

src/hnd/anno/fmtAnnosAsSinglePageCollection.mjs
@@ -36,10 +36,10 @@ const EX = function fmtAnnosAsSinglePageCollection(how) {
 };
 
 
-function replyToRequest(req, how) {
+function replyToRequest(srv, req, how) {
   if (!how.canonicalUrl) {
-    const canonicalUrl = hndUtil.guessOrigReqUrl(req);
-    return replyToRequest(req, { ...how, canonicalUrl });
+    const canonicalUrl = hndUtil.guessOrigReqUrl(srv, req);
+    return replyToRequest(srv, req, { ...how, canonicalUrl });
   }
   return sendFinalTextResponse.json(req, EX(how));
 }

src/hnd/anno/idGet.mjs
@@ -9,7 +9,7 @@ import verifyAnnoIdFormat from './verifyAnnoIdFormat.mjs';
 const namedEqual = equal.named.deepStrictEqual;
 
 
-async function idGet(annoId, req, srv) {
+async function idGet(srv, req, annoId) {
   verifyAnnoIdFormat(annoId);
   const reply = await srv.db.postgresSelect('"details" FROM "anno_data"'
     + ' WHERE "anno_id" = $1 LIMIT 2;', [annoId]);

src/hnd/anno/route.mjs
@@ -21,22 +21,22 @@ Object.assign(EX, {
         'Anno subresource not implemented');
     }
     const [annoId] = urlSubDirs;
-    if (annoId) { return EX.annoIdRoute(annoId, req, srv); }
-    return EX.emptyIdRoute(req, srv);
+    if (annoId) { return EX.annoIdRoute(srv, req, annoId); }
+    return EX.emptyIdRoute(srv, req);
   },
 
 
-  async emptyIdRoute(req, srv) {
+  async emptyIdRoute(srv, req) {
     const { method } = req;
-    if (method === 'GET') { return emptyIdGet(req, srv); }
+    if (method === 'GET') { return emptyIdGet(srv, req); }
 
     return httpErrors.badVerb(req);
   },
 
 
-  async annoIdRoute(annoId, req, srv) {
+  async annoIdRoute(srv, req, annoId) {
     const { method } = req;
-    if (method === 'GET') { return idGet(annoId, req, srv); }
+    if (method === 'GET') { return idGet(srv, req, annoId); }
 
     return httpErrors.badVerb(req);
   },

mk-pmb/anno-backend-22

src/hnd/anno/fmtAnnosAsSinglePageCollection.mjs
@@ -36,10 +36,10 @@ const EX = function fmtAnnosAsSinglePageCollection(how) {
 };
 
 
-function replyToRequest(req, how) {
+function replyToRequest(srv, req, how) {
   if (!how.canonicalUrl) {
-    const canonicalUrl = plumb.guessOrigReqUrl(req);
-    return replyToRequest(req, { ...how, canonicalUrl });
+    const canonicalUrl = plumb.guessOrigReqUrl(srv, req);
+    return replyToRequest(srv, req, { ...how, canonicalUrl });
   }
   return sendFinalTextResponse.json(req, EX(how));
 }

src/hnd/anno/idGet.mjs
@@ -9,7 +9,7 @@ import verifyBaseIdFormat from './verifyBaseIdFormat.mjs';
 const namedEqual = equal.named.deepStrictEqual;
 
 
-async function idGet(baseId, req, srv) {
+async function idGet(srv, req, baseId) {
   verifyBaseIdFormat(baseId);
   const reply = await srv.db.postgresSelect('"details" FROM "anno_data"'
     + ' WHERE "base_id" = $1 LIMIT 2;', [baseId]);

src/hnd/anno/route.mjs
@@ -21,22 +21,22 @@ Object.assign(EX, {
         'Anno subresource not implemented');
     }
     const [baseId] = urlSubDirs;
-    if (baseId) { return EX.annoIdRoute(baseId, req, srv); }
-    return EX.emptyIdRoute(req, srv);
+    if (baseId) { return EX.annoIdRoute(srv, req, baseId); }
+    return EX.emptyIdRoute(srv, req);
   },
 
 
-  async emptyIdRoute(req, srv) {
+  async emptyIdRoute(srv, req) {
     const { method } = req;
-    if (method === 'GET') { return emptyIdGet(req, srv); }
+    if (method === 'GET') { return emptyIdGet(srv, req); }
 
     return httpErrors.badVerb(req);
   },
 
 
-  async annoIdRoute(baseId, req, srv) {
+  async annoIdRoute(srv, req, baseId) {
     const { method } = req;
-    if (method === 'GET') { return idGet(baseId, req, srv); }
+    if (method === 'GET') { return idGet(srv, req, baseId); }
 
     return httpErrors.badVerb(req);
   },