mk-pmb/anno-server-22

Centralize redirects, add redirect logging

src/hnd/sess/route.mjs
@@ -1,7 +1,5 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import makeRedirector from 'deviate';
-
 import httpErrors from '../../httpErrors.mjs';
 import requestDebugHandler from '../debugRequest.mjs';
 // import sendFinalTextResponse from '../../finalTextResponse.mjs';
@@ -11,14 +9,14 @@ import hndUtil from '../hndUtil.mjs';
 
 const EX = async function makeSessionRoute() {
 
-  const redirectToWhoami = makeRedirector('whoami');
+  const redirectToWhoami = hndUtil.makeRedirector('whoami');
 
   function sessionHnd(req) {
     const subUrl = hndUtil.getFirstAsteriskUrlPart(req);
     if (subUrl === 'login') {
       // This route is meant to be protected by a mandatory login
       // requirement in a reverse proxy.
-      return redirectToWhoami(req, req.res, req.next);
+      return redirectToWhoami(req);
     }
     if (subUrl === 'whoami') {
       return requestDebugHandler(req);

mk-pmb/anno-backend-22

src/hnd/sess/route.mjs
@@ -1,7 +1,5 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import makeRedirector from 'deviate';
-
 import httpErrors from '../../httpErrors.mjs';
 import requestDebugHandler from '../util/debugRequest.mjs';
 // import sendFinalTextResponse from '../../finalTextResponse.mjs';
@@ -11,14 +9,14 @@ import plumb from '../util/miscPlumbing.mjs';
 
 const EX = async function makeSessionRoute() {
 
-  const redirectToWhoami = makeRedirector('whoami');
+  const redirectToWhoami = plumb.makeRedirector('whoami');
 
   function sessionHnd(req) {
     const subUrl = plumb.getFirstAsteriskUrlPart(req);
     if (subUrl === 'login') {
       // This route is meant to be protected by a mandatory login
       // requirement in a reverse proxy.
-      return redirectToWhoami(req, req.res, req.next);
+      return redirectToWhoami(req);
     }
     if (subUrl === 'whoami') {
       return requestDebugHandler(req);